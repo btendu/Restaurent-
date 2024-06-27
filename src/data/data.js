97d0eb6ac31a977c0ef131385734777aafe43fed
btendu <goyalbhartendu@gmail.com>
2024-06-27 15:21:43 +0530

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description: "South indian ka best dosa khayga tu khate rhe jayega",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description: "Punjabi chole dil bole hole hole ",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description: "Idli or sambhar ki kya aukat  jalebi fada ke samne ",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Masala dosa masale se bhra italy  bengal ormumbaikevadapau ka mix masala  100 din purana woh alg",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Veg logo ka mutton chicken thats my fav as well beacuse i am also vegtarian majboori tera ghar jaygega isko khridne mai",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "sbse best h fafda jalebi aayeye humre yha ka best fafda jalebi khayei mumbai se jethalal bhi idhr hi ata hai ",
+    image: Gujrati,
+    price: 500,
+  },
+];
